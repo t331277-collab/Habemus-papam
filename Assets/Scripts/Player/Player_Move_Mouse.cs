7e4e2b726d297971d7c6b25ba097de0ddd0229a1
t331277-collab <t331277@gmail.com>
2025-11-22 01:21:37 +0900

    마우스 이동 로직 변경입니다.
    변경사항 : Player_Move_Mouse.cs
    
    정지상태일때 위치고정을 해제하여 이제 두 스크립트 동시에 사용할 수 있습니다.

--- Assets/Scripts/Player/Player_Move_Mouse.cs
+++ Assets/Scripts/Player/Player_Move_Mouse.cs
@@ -22,8 +22,11 @@ public class Player_Move_Mouse : MonoBehaviour
 
     void Update()
     {
+        //마우스로 이동할 좌표 입력
         HandleClick();
-        SetAgentPosition();
+
+        //도착시 커서 이펙트 Destroy, 위치 고정 해제
+        Arrived();
     }
 
     void HandleClick()
@@ -33,6 +36,8 @@ public class Player_Move_Mouse : MonoBehaviour
             Vector3 world = cam.ScreenToWorldPoint(Input.mousePosition);
             world.z = zDepth;
 
+
+            //마우스 커서 이미지
             if(last != null)
             {
                 Destroy(last);
@@ -41,12 +46,25 @@ public class Player_Move_Mouse : MonoBehaviour
             last = Instantiate(prefab, world, Quaternion.identity);
 
             target = world;
+
+            agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
         }
 
 
     }
-    void SetAgentPosition()
+   
+
+    void Arrived()
     {
-        agent.SetDestination(new Vector3(target.x, target.y, transform.position.z));
+        //경로 탐색이 계산중이지 않고 and (목적지까지의 남은거리가 agent.stoppingDistance 보다 작거나 같을때)
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance) //agent.stoppingDistance의 default = 0
+        {
+            //유효한 경로가 있지 않거나 agent 의 속도가 0.001f 보다 작다(정지상태로 추정)면 커서 프리팹 Destroy 및 agent의 목적지 해제
+            if(!agent.hasPath || agent.velocity.sqrMagnitude < 0.001f)
+            {
+                Destroy(last);          //커서 삭제
+                agent.ResetPath();      //목적지 도착시 위치 고정하지 않음
+            }
+        }
     }
 }
